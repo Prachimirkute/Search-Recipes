519fe44dcbc659437bb7b10b5ce1fe12da16a7c9
Prachimirkute <prachimirkute27@gmail.com>
2024-06-05 22:21:59 +0100

    first commit

--- src/views/MealDetails.vue
+++ src/views/MealDetails.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="max-w-[800px] mx-auto p-8">
-    <h1 class="text-4xl font-bold mb-5 text-orange-500">{{ meal.strMeal }}</h1>
+    <h1 class="text-4xl font-bold mb-5 text-purple-500">{{ meal.strMeal }}</h1>
     <img :src="meal.strMealThumb" :alt="meal.strMeal" class="max-w-[100%]">
     <div class="grid grid-cols-1 sm:grid-cols-3 text-lg py-2">
       <div>
@@ -18,7 +18,7 @@
       {{ meal.strInstructions }}
     </div>
 
-    <div class="grid grid-cols-1 sm:grid-cols-2">
+    <div class="grid grid-cols-1">
       <div>
         <h2 class="text-2xl font-semibold mb-2">Ingredients</h2>
         <ul>
